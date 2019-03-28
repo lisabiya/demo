b3ff54b72cdfb7a58035008e55009dd8fc4397b6
wangkaixun <1526446017@qq.com>
2019-03-28 16:14:40 +0800

    动态代理

--- app/src/main/java/com/example/administrator/myapplication/rxpopup/RxPopupManager.kt
+++ app/src/main/java/com/example/administrator/myapplication/rxpopup/RxPopupManager.kt
@@ -3,23 +3,84 @@ package com.example.administrator.myapplication.rxpopup
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.FragmentManager
-import com.example.administrator.myapplication.R
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
 
 
 fun showSimplePop(fragmentManager: FragmentManager?) {
     val rxPopup = object : RxPopup() {
         override fun onViewCreated(view: View) {
             setCancelable(true)
-            val message = view.findViewById<TextView>(R.id.tvMessage)
-            setViewOnClick(R.id.ivClose) {
+            val message = view.findViewById<TextView>(com.example.administrator.myapplication.R.id.tvMessage)
+            setViewOnClick(com.example.administrator.myapplication.R.id.ivClose) {
                 dismiss()
             }
-            setViewOnClick(R.id.btSet) {
+            setViewOnClick(com.example.administrator.myapplication.R.id.btSet) {
                 message.text = String.format("btSet")
             }
         }
     }
 
     if (fragmentManager != null)
-        rxPopup.showFragment(fragmentManager, R.layout.fragment_dialog)
+        rxPopup.showFragment(fragmentManager, com.example.administrator.myapplication.R.layout.fragment_dialog)
+}
+
+interface Subject {
+
+    fun hello(str: String)
+
+    fun bye(): String
+}
+
+class RealSubject : Subject {
+    override fun hello(str: String) {
+        System.out.println("Hello  " + str);
+    }
+
+    override fun bye(): String {
+        System.out.println("Goodbye");
+        return "Over";
+    }
+}
+
+fun main() {
+    // 我们要代理的真实对象
+    val realSubject = RealSubject()
+
+    val subject1 = ProxyFactory(realSubject).proxyInstance as Subject;
+    //执行方法
+    subject1.hello("kitty")
+    subject1.bye();
+}
+
+/**
+ * 创建动态代理对象
+ * 动态代理不需要实现接口,但是需要指定接口类型
+ */
+class ProxyFactory(private val target: Any) {
+
+    //给目标对象生成代理对象
+    //执行目标对象方法
+    val proxyInstance: Any
+        get() = Proxy.newProxyInstance(
+                target.javaClass.classLoader,
+                target.javaClass.interfaces,
+                object : InvocationHandler {
+                    @Throws(Throwable::class)
+                    override fun invoke(proxy: Any, method: Method, args: Array<Any>?): Any? {
+                        println("开始事务2")
+                        //执行目标对象方法
+                        val returnValue: Any? = if (args == null) {
+                            method.invoke(target)
+                        } else {
+                            method.invoke(target, *args)
+                        }
+                        println("提交事务2")
+                        println()
+                        return returnValue
+                    }
+                }
+        )
+
 }
---
+++ app/src/main/java/com/example/administrator/myapplication/utils/Invocation.java
@@ -0,0 +1,32 @@
+package com.example.administrator.myapplication.utils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class Invocation implements InvocationHandler {
+    // 这个就是我们要代理的真实对象
+    private Object subject;
+
+    // 构造方法，给我们要代理的真实对象赋初值
+    public Invocation(Object subject) {
+        this.subject = subject;
+    }
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 在代理真实对象前我们可以添加一些自己的操作
+        System.out.println("Before method");
+
+        System.out.println("Call Method: " + method);
+
+        // 当代理对象调用真实对象的方法时，其会自动的跳转到代理对象关联的handler对象的invoke方法来进行调用
+        Object obj = method.invoke(subject, args);
+
+        // 在代理真实对象后我们也可以添加一些自己的操作
+        System.out.println("After method");
+        System.out.println("返回值"+obj);
+        System.out.println();
+        return obj;
+    }
+}
